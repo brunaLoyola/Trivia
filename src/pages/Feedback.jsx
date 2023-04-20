36eb399ae475cd7282a26e2d963a9ebb66d80415
SherlyAmorim <sherlyamorim@gmail.com>
2023-04-20 17:59:10 -0300

    Iniciando Req 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -3,10 +3,21 @@ import { connect } from 'react-redux';
 import Header from '../componentes/Header';
 
 class Feedback extends Component {
+  feedbackCondition = () => {
+    const scoreMin = 3;
+    const bad = 'Could be better...';
+    const god = 'Well Done!';
+    if (player.score < scoreMin) {
+      return bad;
+    }
+    return god;
+  };
+
   render() {
     return (
       <div>
         <Header />
+        <h2 data-testid="feedback-text">{ this.feedbackCondition }</h2>
       </div>
     );
   }
